80617ba53c6db99f7585da3251d8530279ec3e2b
Polismun <polismun9@gmail.com>
2026-03-19 00:32:06 +0100

    .

--- server/go.mod
+++ server/go.mod
@@ -4,16 +4,4 @@ go 1.25.0
 
 require github.com/klauspost/compress v1.18.4
 
-require (
-	github.com/go-rod/rod v0.116.2
-	github.com/go-rod/stealth v0.4.9
-	github.com/nwaples/rardecode/v2 v2.2.2
-)
-
-require (
-	github.com/ysmood/fetchup v0.2.3 // indirect
-	github.com/ysmood/goob v0.4.0 // indirect
-	github.com/ysmood/got v0.40.0 // indirect
-	github.com/ysmood/gson v0.7.3 // indirect
-	github.com/ysmood/leakless v0.9.0 // indirect
-)
+require github.com/nwaples/rardecode/v2 v2.2.2
--- server/main.go
+++ server/main.go
@@ -37,9 +37,7 @@ const (
 	maxDemoSaveBytes = 200 << 20 // 200 MB for parsed JSON
 	rateMaxRead          = 60        // 60 req/min per IP for demo reads
 	demosDir             = "demos"
-	maxDownloadBytes     int64 = 2 << 30     // 2 GB download limit for /parse-url
 	maxDecompressedBytes int64 = 2 << 30     // 2 GB decompressed limit (RAR bomb protection)
-	downloadTimeout      = 5 * time.Minute   // HTTP download timeout for /parse-url
 )
 
 // Semaphore limits concurrent parser processes
@@ -97,7 +95,7 @@ func main() {
 	// Cleanup stale temp files from previous crashes
 	cleanupTempFiles()
 
-	http.HandleFunc("/parse-url", handleParseURL)
+	http.HandleFunc("/parse-multi", handleParseMulti)
 	http.HandleFunc("/parse", handleParse)
 	http.HandleFunc("/demo/save", handleDemoSave)
 	http.HandleFunc("/demo/", handleDemoRoute)
